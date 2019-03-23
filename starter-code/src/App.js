600c43348dbeec1628e5de191fcb65b682d4c8d5
guilhermeaeme <guilhermeaeme@gmail.com>
2019-03-23 15:35:18 -0300

    Iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import NavBar from "../components/NavBar";
+import FormField from "../components/FormField";
 
 class App extends Component {
   render() { 
     return (
-      <NavBar />
+      <Fragment>
+        <NavBar />
+
+        <div className="container">
+          <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+          <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+        </div>
+      </Fragment>
     );
   }
 }
